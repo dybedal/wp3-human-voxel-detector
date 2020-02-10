eba0831469a965882b13c70f7b5e7a2df2c2e855
Joacim Dybedal <1422489+dybedal@users.noreply.github.com>
2020-02-10 12:41:34 +0100

    Rewritten to (for now) just sync and merge six point clouds.

--- src/main.cpp
+++ src/main.cpp
@@ -7,94 +7,126 @@
 #include <fstream>
 
 #include <ros/ros.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
 
-#include "wp3_decompressor/decompressor.h"
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
+
+using namespace message_filters;
+
+typedef pcl::PointXYZI PCL_PointType;
+typedef pcl::PointCloud<PCL_PointType> PointCloudXYZI;
+typedef sync_policies::ApproximateTime<PointCloudXYZI, PointCloudXYZI, PointCloudXYZI, PointCloudXYZI, PointCloudXYZI, PointCloudXYZI> MySyncPolicy;
+
+
 
 // Setup
-#define MIN_POINTS 3 //minimum number of points in compressed voxels
 #define ROS_RATE 60
 
 void killHandler(int)
 {
-  ROS_INFO("%s","Shutdown request received.");
-  ros::NodeHandle nh;
-  ROS_INFO("%s","Terminating nodehandle.");
-  nh.shutdown();
-  ROS_INFO("%s","Terminating rosnode.");
-  ros::shutdown();
+	ROS_INFO("%s","Shutdown request received.");
+	ros::NodeHandle nh;
+	ROS_INFO("%s","Terminating nodehandle.");
+	nh.shutdown();
+	ROS_INFO("%s","Terminating rosnode.");
+	ros::shutdown();
 }
+
+class PointCloudMerger
+{
+public:
+	PointCloudMerger() : 
+		nh{"~"},
+		pc1_sub{nh, "pointcloud1_in", 10},
+		pc2_sub{nh, "pointcloud2_in", 10},
+		pc3_sub{nh, "pointcloud3_in", 10},
+		pc4_sub{nh, "pointcloud4_in", 10},
+		pc5_sub{nh, "pointcloud5_in", 10},
+		pc6_sub{nh, "pointcloud6_in", 10},
+		sync{MySyncPolicy(100), pc1_sub, pc2_sub, pc3_sub, pc4_sub, pc5_sub, pc6_sub}
+
+	{
+		pub = nh.advertise<PointCloudXYZI>("pointcloud_out", 1);
+	
+		sync.registerCallback(boost::bind(&PointCloudMerger::callback, this, _1, _2, _3, _4, _5, _6));
+	}
+
+	void callback(const PointCloudXYZI::ConstPtr & cloud1, const PointCloudXYZI::ConstPtr & cloud2, const PointCloudXYZI::ConstPtr & cloud3,
+		const PointCloudXYZI::ConstPtr & cloud4, const PointCloudXYZI::ConstPtr & cloud5, const PointCloudXYZI::ConstPtr & cloud6)
+	{
+	
+		PointCloudXYZI out_cloud;
+		out_cloud.width = cloud1->width;
+		out_cloud.height = cloud1->height;
+		out_cloud.header.frame_id = cloud1->header.frame_id;
+		
+		out_cloud = *cloud1;
+		out_cloud += *cloud2;
+		out_cloud += *cloud3;
+		out_cloud += *cloud4;
+		out_cloud += *cloud5;
+		out_cloud += *cloud6;
+
+		pub.publish(out_cloud);
+	}
+
+	void shutdown()
+	{
+		nh.shutdown();
+	}
+
+
+private:
+	ros::NodeHandle nh;
+	ros::Publisher pub;
+	
+	message_filters::Subscriber<PointCloudXYZI> pc1_sub;
+	message_filters::Subscriber<PointCloudXYZI> pc2_sub;
+	message_filters::Subscriber<PointCloudXYZI> pc3_sub;
+	message_filters::Subscriber<PointCloudXYZI> pc4_sub;
+	message_filters::Subscriber<PointCloudXYZI> pc5_sub;
+	message_filters::Subscriber<PointCloudXYZI> pc6_sub;
+
+	// ApproximateTime takes a queue size as its constructor argument, hence MySyncPolicy(10)
+	Synchronizer<MySyncPolicy> sync;
+	
+};
+
+
 
 int main(int argc, char **argv)
 {
-  // Initialize ROS
-  ros::init(argc, argv, "wp3_decompressor", ros::init_options::NoSigintHandler | ros::init_options::AnonymousName);
-  ros::NodeHandle nh("~");
+	// Initialize ROS
+	ros::init(argc, argv, "wp3_decomp_multi", ros::init_options::NoSigintHandler | ros::init_options::AnonymousName);
+	
 
-  signal(SIGINT, killHandler);
-  signal(SIGTERM, killHandler);
-
-  ros::Rate loopRate(ROS_RATE);
-
-  // Multithreaded spinner
-  //    	ros::AsyncSpinner spinner(8);
-  //    	spinner.start();
-  //    	usleep(1000000); // give the spinner some time to start (1000 ms)
-
-  if(!nh.hasParam("sensor_name"))
-    ROS_ERROR("%s","Missing _sensor_name:=<name> parameter! Shutting down...");
-  else if(!nh.hasParam("filter_value"))
-    ROS_ERROR("%s","Missing _filter_value:=<intensity> parameter! Shutting down...");
-  else if(!nh.hasParam("sensor_type"))
-    ROS_ERROR("%s","Missing _sensor_type:=<0: Kinect, 1: Velodyne> parameter! Shutting down...");
-  else{
-
-    std::string outputTopic;
-    std::string inputTopic;
-    std::string sensorName;
-    std::string sensorFrame;
-    float filterValue;
-    int sensorType;
+	signal(SIGINT, killHandler);
+	signal(SIGTERM, killHandler);
 
 
-    nh.getParam("sensor_name", sensorName);
-    nh.getParam("filter_value", filterValue);
-    nh.getParam("sensor_type", sensorType);
-    nh.param<std::string>("sensor_frame", sensorFrame, "world");
+	PointCloudMerger merger;
 
-    switch(sensorType) {
-    case 0: // Kinect
+	ros::Rate loopRate(ROS_RATE);
 
-      ROS_INFO("Starting node using KINECT V2");
-      outputTopic = "/master/"+sensorName+"/kinect_decomp";
-      inputTopic = "/"+sensorName+"/wp3/kinect_comp";
-      break;
+	
 
-    case 1: // Velodyne
-      ROS_INFO("Starting node using VELODYNE");
-      outputTopic = "/master/"+sensorName+"/velodyne_decomp";
-      inputTopic = "/"+sensorName+"/wp3/velodyne_comp";
-      break;
+	while(ros::ok()){
+		ros::spinOnce();
+		loopRate.sleep();
+	}
+ 
 
-    } // End switch (sensorType)
-
-    ros::Duration(1.0).sleep();
-
-    // Start decompressor
-    wp3::CloudDecompressor decompressor(outputTopic, inputTopic, sensorFrame, filterValue, false);
+	if(ros::ok()){
+		merger.shutdown();
+		ros::shutdown();
+	}
 
 
-    while(ros::ok()){
-      ros::spinOnce();
-      loopRate.sleep();
-    }
-  }
-
-  if(ros::ok()){
-    nh.shutdown();
-    ros::shutdown();
-  }
-
-
-  ROS_INFO("%s","Shutdown complete.");
-  return 0;
+ 	ROS_INFO("%s","Shutdown complete.");
+	return 0;
 }
